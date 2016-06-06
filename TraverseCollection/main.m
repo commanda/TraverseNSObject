5b7b0744c2ba63b960852bc772b8c1117f8f9432
Amanda Wixted <amanda@amandawixted.com>
2016-06-06 15:18:01 -0500

    Initial Commit

---
+++ TraverseCollection/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TraverseCollection
+//
+//  Created by Amanda Wixted on 6/6/16.
+//  Copyright © 2016 Amanda Wixted. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
