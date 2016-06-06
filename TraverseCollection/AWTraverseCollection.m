86b9980f2d28a7be7242a3d74f80bc2367cfd695
Amanda Wixted <amanda@amandawixted.com>
2016-06-06 15:36:56 -0500

    done

--- TraverseCollection/AWTraverseCollection.m
+++ TraverseCollection/AWTraverseCollection.m
@@ -2,7 +2,6 @@
 //  AWTraverseCollection.m
 //  TraverseCollection
 //
-//  Created by Amanda Wixted on 6/6/16.
 //  Copyright © 2016 Amanda Wixted. All rights reserved.
 //
 
@@ -10,10 +9,30 @@
 
 @implementation AWTraverseCollection
 
-
-+(void)traverseCollection:(NSObject *)collection applyBlockToLeaves:(ApplyToLeaf)block
++ (void)traverseCollection:(NSObject *)collection applyBlockToLeaves:(ApplyToLeaf)block
 {
-    
+    if ([collection isKindOfClass:[NSDictionary class]])
+    {
+        for (NSString* value in [(NSDictionary *)collection allValues])
+        {
+            [self traverseCollection:value applyBlockToLeaves:block];
+        }
+    }
+    else if ([collection isKindOfClass:[NSArray class]])
+    {
+        for (id child in (NSArray *)collection)
+        {
+            [self traverseCollection:child applyBlockToLeaves:block];
+        }
+    }
+    else
+    {
+        // We found a leaf
+        if(block)
+        {
+            block(collection);
+        }
+    }
 }
 
 @end
--- TraverseCollection/main.m
+++ TraverseCollection/main.m
@@ -2,7 +2,6 @@
 //  main.m
 //  TraverseCollection
 //
-//  Created by Amanda Wixted on 6/6/16.
 //  Copyright © 2016 Amanda Wixted. All rights reserved.
 //
 
